0c259aeca8a5ed8960096d52352b9f303c580b4b
AngryWolf <qrer71@yandex.ru>
2024-02-18 16:52:40 +0300

    Update main

---
+++ src/tasks/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from tasks.api import routers
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    for router in routers:
+        app.include_router(router)
+
+    return app
+
+
+app = create_app()
