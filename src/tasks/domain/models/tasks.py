5f6b7a776f4f30521f12fb2b8cd7c787413c23fc
AngryWolf <qrer71@yandex.ru>
2024-02-18 17:53:41 +0300

    Update models

--- src/tasks/domain/models/tasks.py
+++ src/tasks/domain/models/tasks.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from tasks.domain.models.association_tables import users_tasks
 from tasks.infrastructure.db import Base
+from tasks.domain.schemas import TaskGet
 
 
 class Task(Base):
@@ -10,5 +11,18 @@ class Task(Base):
     description: Mapped[str] = mapped_column(nullable=False)
     users: Mapped[list['User']] = relationship(
         secondary=users_tasks,
-        back_populates='tasks'
+        back_populates='tasks',
+        lazy='selectin'
     )
+
+    def to_read_model(self) -> TaskGet:
+        if not self.users:
+            return TaskGet(
+                name=self.name,
+                description=self.description
+            )
+        return TaskGet(
+            name=self.name,
+            description=self.description,
+            users=[user.to_read_model() for user in self.users]
+        )
--- src/tasks/domain/models/users.py
+++ src/tasks/domain/models/users.py
@@ -1,7 +1,11 @@
+from collections import Generator
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from tasks.domain.models.association_tables import users_tasks
 from tasks.infrastructure.db import Base
+
+from tasks.domain.schemas import UserGet
 
 
 class User(Base):
@@ -11,3 +15,6 @@ class User(Base):
         secondary=users_tasks,
         back_populates='users'
     )
+
+    def to_read_model(self) -> UserGet:
+        return UserGet(user_id=self.user_id)
